d9b71e5876c800f6d410f71fe8a6b0ec8ab9af78
Gislany Araujo <gislany.sa@gmail.com>
2025-09-13 11:39:39 -0300

    ajuste pages

--- sigo-frontend/src/App.jsx
+++ sigo-frontend/src/App.jsx
@@ -1,34 +1,20 @@
 import { useState } from 'react'
 import './App.css'
-import logoSigo from './assets/logosigo.svg'
-import Header from './components/Header';
+import Header from './components/Header'
+import Register from './components/Register'
+import Login from './components/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLogin, setIsLogin] = useState(true)
 
   return (
     <>
-    <Header/>
-    <div className="page-container">
-      <div className="logo-section">
-        <img src={logoSigo} alt="Logo do SIGO"/>
-        <h2>Sistema Integrado de Gestão de Ocorrências</h2>
-        <p>A plataforma integrada que moderniza o registro e a gestão das ocorrências em Pernambuco</p>
-      </div>
-
-      <div className="login-section">
-        <h2>Digite sua Matrícula para acessar o sistema</h2>
-        <label>Matrícula</label>
-        <input type="text" placeholder="Digite sua Matrícula" />
-
-        <label>Senha</label>
-        <input type="password" placeholder="Digite sua Senha"/>
-
-        <button type="submit">Entrar</button>
-        <p>Primeira vez aqui? <a href='#'>Registre-se</a></p>
-      </div>
-
-    </div>
+      <Header/>
+      {isLogin ? (
+        <Login onRegister={() => setIsLogin(false)} />
+      ) : (
+        <Register onBackToLogin={() => setIsLogin(true)} />
+      )}
     </>
   )
 }
